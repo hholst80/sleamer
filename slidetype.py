5a4178ee376140dfadcee27a370b759768c13872
Henrik Holst <hholst80@gmail.com>
2017-05-09 23:18:15 +0200

    WIP: prototype

---
+++ slidetype.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('N', type=int)
+args = parser.parse_args()
+
+pp = sys.stdin.read().split('\n\n')
+p = pp[args.N-1].strip()
+for l in p.split('\n'):
+    if l[0] != '#':
+        continue
+    if '@' in l:
+        print(l[l.index('@')+1:])
